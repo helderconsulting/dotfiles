e2ebeb9d60d1f82cd3379c14b63471467e4870da
Tim Alaerts <tim.alaerts@helderconsulting.com>
2026-03-14 19:10:50 +0100

    created query pagination

--- nvim/lua/packs/db.lua
+++ nvim/lua/packs/db.lua
@@ -1,6 +1,8 @@
 local result_buffer = nil
 local result_window = nil
 local query_window = nil
+local page_index = 0
+local page_size = 2
 local connection = "mongodb://localhost:27017"
 
 local open_window = function()
@@ -42,7 +44,71 @@ vim.api.nvim_create_user_command("QueryDatabase", function(opts)
 	end
 end, { range = true })
 
-vim.api.nvim_del_user_command("QueryCollection", function(opts) end)
+local list = function(collection)
+	open_window()
+	local list = string.format('db.getCollection("%s").find({}).skip(%d).limit(%d)', collection[1], page_index, page_size)
+	show_result({ list })
+	if query_window then
+		vim.api.nvim_set_current_win(query_window)
+	end
+end
+
+vim.api.nvim_create_user_command("QueryCollection", function()
+	local ok, fzf = pcall(require, "fzf-lua")
+	if not ok then
+		vim.notify("fzf-lua not available", vim.log.levels.WARN)
+		return
+	end
+
+	local query = "db.getCollectionNames()"
+
+	local result = vim.fn.system("mongosh --quiet '" .. connection .. "' --eval '" .. query .. "'")
+	local json = result:gsub("'", '"')
+	local c = vim.json.decode(json)
+	local lines = vim.split(result, "\n", { trimempty = true })
+
+	local collections = {}
+	for _, line in ipairs(lines) do
+		table.insert(collections, line)
+	end
+	if #collections == 0 then
+		vim.notify("No collections found", vim.log.levels.INFO)
+		return
+	end
+	fzf.fzf_exec(c, {
+		prompt = "Collections> ",
+		winopts = { height = 0.35, width = 1.00, row = 1.00, border = "none", preview = { hidden = "hidden" } },
+		actions = {
+			["enter"] = function(collection)
+				list(collection)
+				vim.keymap.set("n", "<leader>}", function()
+					page_size = page_size + 1
+					list(collection)
+				end, { noremap = true, buffer = true, desc = "increase page size" })
+				vim.keymap.set("n", "<leader>{", function()
+					local next_size = page_size - 1
+					if next_size > 0 then
+						page_size = next_size
+					end
+					list(collection)
+				end, { noremap = true, buffer = true, desc = "decrease page size" })
+
+				vim.keymap.set("n", "<leader>]", function()
+					page_index = page_index + 1
+					list(collection)
+				end, { noremap = true, buffer = true, desc = "next page" })
+
+				vim.keymap.set("n", "<leader>[", function()
+					local next_index = page_index - 1
+					if next_index > 0 then
+						page_index = next_index
+					end
+					list(collection)
+				end, { noremap = true, buffer = true, desc = "previous page" })
+			end,
+		},
+	})
+end, { range = true })
 
 vim.keymap.set("v", "<leader>qd", ":QueryDatabase<cr>", { desc = "queries the database with selection" })
 vim.keymap.set("n", "<leader>ql", ":QueryCollection<cr>", { desc = "lists the data of selected collection" })
